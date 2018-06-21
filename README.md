3a0c2b039211656a8aeb0d50de32ca90bf52d569
tfili001 <tfili001@odu.edu>
2018-06-21 17:58:00 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Tpsi is a command line tool for fetching, extracting,
+and compiling GitHub repositories.
---
+++ links.py
@@ -0,0 +1,7 @@
+search_prefix = "https://api.github.com/search/repositories?q="
+tar_url = "https://api.github.com/repos/hishamhm/htop/tarball/master"
+
+#	Direct Repo Tar
+# https://api.github.com/repos/octokit/octokit.rb/tarball
+#curl -L https://api.github.com/repos/hishamhm/htop/tarball/master > htop.tar.gz
+#curl -L https://api.github.com/repos/"username"/"name"/tarball/master > htop.tar.gz
